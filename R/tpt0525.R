93200bac27e93bd4bc975891626ab526f9aa4095
Emha Alwi Al Makky <emhaalwialmakky@gmail.com>
2025-10-27 08:52:40 +0800

    add tpt0252

---
+++ R/tpt0525.R
@@ -0,0 +1,16 @@
+#' Data Tingkat Pengangguran Terbuka (TPT) Indonesia
+#'
+#' Dataset ini berisi TPT Indonesia dalam satuan persen (%) yang dirilis oleh BPS RI setiap bulan Februari dan Agustus.
+#'
+#' @format Sebuah data frame dengan 41 baris dan 2 kolom:
+#' \describe{
+#'   \item{bulan}{Bulan Feb/Agustus}
+#'   \item{tpt}{Tingkat Pengangguran Terbuka (%)}
+#' }
+#'
+#' @source BPS Indonesia, 2025
+#'
+#' @examples
+#' data(tpt0525)
+#' head(tpt0525)
+"tpt0525"
